5ed471f8b9f141d9440b2ef3d3fc234d06efa111
Anton Mitsengendler <a.mic@rtc.ru>
2019-08-30 11:36:24 +0300

    Add project files.

---
+++ Nventory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
