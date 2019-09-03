b1f70f4b61680a38f5e74d877d3406860dde5524
Anton Mitsengendler <a.mic@rtc.ru>
2019-09-04 02:19:39 +0300

    users manager controller datables js intergration

---
+++ Nventory/Controllers/ManageUsersController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nventory.Models;
+
+namespace Nventory.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ManageUsersController : Controller
+    {
+        private UserManager<NventoryUser> _userManager;
+        public ManageUsersController(UserManager<NventoryUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        // GET: ManageUsers
+        public async Task<IActionResult> Index()
+        {            
+            return View(await _userManager.Users.ToListAsync());
+        }
+
+        //// GET: ManageUsers/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: ManageUsers/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: ManageUsers/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(IndexAsync));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ManageUsers/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ManageUsers/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(IndexAsync));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ManageUsers/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ManageUsers/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(IndexAsync));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Nventory/wwwroot/js/tables.js
@@ -0,0 +1,15 @@
+﻿//jQuery(document).ready(function ($) {
+
+//    "use strict";
+
+//    [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
+//        new SelectFx(el);
+//    });
+
+//    jQuery('.selectpicker').selectpicker;
+
+//    $(document).ready(function () {
+//        $('#data-table').DataTable();
+//    });
+
+//});
