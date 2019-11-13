3c83faff6cdc210a4969bd487565135e10006b0e
Anton Mitsengendler <a.mic@rtc.ru>
2019-11-13 21:41:42 +0300

    users import and export

--- Nventory/Controllers/ManageUsersController.cs
+++ Nventory/Controllers/ManageUsersController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -105,6 +110,76 @@ namespace Nventory.Controllers
             return PartialView(new UserViewModel(user));
         }    
 
+        [HttpGet]
+        public async Task<IActionResult> Export()
+        {
+            var users = await _usersRepository.GetUsersAsync();
+
+            using(var ms = new MemoryStream())
+            using(var writer = new StreamWriter(ms, Encoding.UTF8))
+            using(var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(users);             
+                var cd = new System.Net.Mime.ContentDisposition
+                {
+                    FileName = "users.csv",
+                    Inline = false
+                };                
+                Response.Headers.Add("Content-Disposition", cd.ToString());
+                await writer.FlushAsync();                
+                var data = ms.ToArray();
+                return File(data, "application/csv");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return PartialView();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Import(IFormFile formFile)
+        {
+            if(formFile != null)
+            {
+                using (var stream = formFile.OpenReadStream())
+                using (var reader = new StreamReader(stream))
+                using (var csv = new CsvReader(reader))
+                {                    
+                    csv.Configuration.HeaderValidated = null;
+                    csv.Configuration.MissingFieldFound = null;
+                    var users = csv.GetRecords<NUser>();
+                    foreach(var user in users
+                        .Where(u => !string.IsNullOrWhiteSpace(u.Password) && !string.IsNullOrWhiteSpace(u.UserName)))
+                    {                                                 
+                        await _usersRepository.CreateUserAsync(user,user.Password);
+                    }
+                }
+            }            
+            return RedirectToAction(nameof(Index));
+        }
+
+        struct NUser : INventoryUser
+        {
+            public string Id { get; set; }            
+            
+            public string UserName { get; set; }
+            
+            public string Password { get; set; }
+            
+            public string Email { get; set; }
+            
+            public string Name { get; set; }
+            
+            public string Surname { get; set; }
+            
+            public string Patronymic { get; set; }
+            
+            public string StaffNumber { get; set; }
+            public IList<INventoryRole> Roles { get; set; }
+        }
 
     }
 }
